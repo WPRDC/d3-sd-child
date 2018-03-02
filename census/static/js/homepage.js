c9c6ea018187612f602ae56e026aaf444a349ee3
Joseph Bosire <kashboss@gmail.com>
2018-03-02 15:56:57 +0300

    Fix nav bug when not closed
    Signed-off-by: Joseph Bosire <kashboss@gmail.com>

--- census/static/js/homepage.js
+++ census/static/js/homepage.js
@@ -11,42 +11,34 @@ navIcon.onclick = () => {
     }
 }
 
-// $(window).scroll(() => {
-//     if ($(window).scrollTop() > 600 && isNavOpen === true) {
-//         $(nav).css('display', 'none');
-//         isNavOpen = false;
-//     }
-// })
-
 
 $('.nav-link').click(() => {
     $(nav).css('display', 'none');
     isNavOpen = false;
-});
+})
+;
 
 
-let desktopToggle = () => {
+let desktopToggle = () =>
+{
+    console.log(isNavOpen, "Status of FLAG");
     if (!isNavOpen) {
-        console.log('here');
-        $('body').animate({
-            scrollTop: 0
-        }, 'fast')
+        $("html, body").animate({ scrollTop: 0 }, "slow");
         $(nav).slideDown(600);
         isNavOpen = true;
     } else {
         $(nav).slideUp(700);
         isNavOpen = false;
     }
-}
+};
 
-let mobileToggle = () => {
+let mobileToggle = () =>
+{
     let contentWidth;
 
     if (!isNavOpen) {
         contentWidth = $('.main-container').width();
-
         $('.main-container').css('width', contentWidth);
-
 
         $(".parent-container").animate({
             "marginLeft": ["-90%"]
@@ -69,4 +61,37 @@ let mobileToggle = () => {
         isNavOpen = false;
     }
 
-}
+};
+
+let hideNav = () => {
+    let didScroll;
+    let lastScrollTop = 0;
+    let delta = 5;
+    let navbarHeight = $('#nav').outerHeight();
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        let st = $(this).scrollTop();
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+        // If they scrolled down and are past the navbar,
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            // We want to hide the whole nav
+            $(nav).css('display', 'none');
+            isNavOpen = false;
+        }
+        lastScrollTop = st;
+    }
+};
+
+hideNav();
